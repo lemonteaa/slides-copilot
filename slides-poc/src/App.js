40aec1982d298ebb7ad9c44c7199f58fc02e9f3d
lemonteaa <lemontea.Tom@gmail.com>
2024-06-18 00:09:11 +0800

    (llama3-70b) layout quick gen

--- slides-poc/src/App.js
+++ slides-poc/src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+//import React from 'react';
+//import { Grid, Toolbar, Typography } from '@material-ui/core';
+
+import * as React from 'react';
+import { Grid, Toolbar, Typography } from '@mui/material';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Typography variant="h4" component="h1">
+          Title
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Toolbar>
+          {/* Add toolbar items here */}
+          <Typography variant="h6">Toolbar</Typography>
+        </Toolbar>
+      </Grid>
+      <Grid item xs={4}>
+        <Typography variant="h6">Column 1</Typography>
+        {/* Add content for column 1 here */}
+      </Grid>
+      <Grid item xs={4}>
+        <Typography variant="h6">Column 2</Typography>
+        {/* Add content for column 2 here */}
+      </Grid>
+      <Grid item xs={4}>
+        <Typography variant="h6">Column 3</Typography>
+        {/* Add content for column 3 here */}
+      </Grid>
+    </Grid>
   );
 }
 
