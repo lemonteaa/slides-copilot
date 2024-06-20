57998a88a79ae1bc28fddd1cfcf4775c25d9469d
lemonteaa <lemontea.Tom@gmail.com>
2024-06-20 10:50:44 +0800

    Basic FabricJS react integration test

--- slides-poc/src/App.js
+++ slides-poc/src/App.js
@@ -5,9 +5,28 @@ import './App.css';
 //import { Grid, Toolbar, Typography } from '@material-ui/core';
 
 import * as React from 'react';
-import { Grid, Toolbar, Typography } from '@mui/material';
+import { Grid, Toolbar, Typography, Card } from '@mui/material';
+
+import { FabricJSCanvas, useFabricJSEditor } from 'fabricjs-react'
 
 function App() {
+  const { editor, onReady } = useFabricJSEditor()
+  const onAddCircle = () => {
+    editor?.addCircle()
+  }
+  const onAddRectangle = () => {
+    editor?.addRectangle()
+  }
+  const myOnReady = (canvas) => {
+    onReady(canvas);
+    canvas.setHeight(500);
+    canvas.setWidth(800);
+  }
+  const onMore = () => {
+    editor?.canvas.setHeight(500);
+    editor?.canvas.setWidth(800);
+  }
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
@@ -27,7 +46,12 @@ function App() {
       </Grid>
       <Grid item xs={4}>
         <Typography variant="h6">Column 2</Typography>
-        {/* Add content for column 2 here */}
+        <Card sx={{border: '1px solid green', borderRadius: 2}} variant="outlined">
+          <button onClick={onAddCircle}>Add circle</button>
+          <button onClick={onAddRectangle}>Add Rectangle</button>
+          <button onClick={onMore}>Test</button>
+          <FabricJSCanvas className="sample-canvas" onReady={myOnReady} />
+        </Card>
       </Grid>
       <Grid item xs={4}>
         <Typography variant="h6">Column 3</Typography>
