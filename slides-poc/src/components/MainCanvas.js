e5070fad186587ad1c596e23466fa97ef1fa02c2
lemonteaa <lemontea.Tom@gmail.com>
2024-06-21 13:10:03 +0800

    [minor] delete unused code

--- slides-poc/src/components/MainCanvas.js
+++ slides-poc/src/components/MainCanvas.js
@@ -13,12 +13,6 @@ export function MainCanvas() {
     const canvasMouseUpCB = useStore((state) => state.canvasMouseUpCB);
     const changeDrawMode = useStore((state) => state.changeDrawMode);
 
-    const onAddCircle = () => {
-        editor?.addCircle()
-    }
-    const onAddRectangle = () => {
-        editor?.addRectangle()
-    }
     const myOnReady = (canvas) => {
         onReady(canvas);
         canvas.setHeight(500);
@@ -40,39 +34,10 @@ export function MainCanvas() {
         canvas.on('mouse:up', canvasMouseUpCB(canvas, originalColor));
     }
 
-    //absolutePointer, mouse:up and down
-    const onMore = () => {
-
-        if (editor) {
-            editor.canvas.selectionColor = 'green'
-            //setDrawing(true)
-            changeDrawMode("rect");
-        }
-
-        //editor?.canvas.setHeight(500);
-        //editor?.canvas.setWidth(800);
-        /*let originalColor = editor.canvas.selectionColor;
-        if (editor) {
-            editor.canvas.selectionColor = 'green';
-            //editor.canvas.selection = false;
-            editor.canvas.on('mouse:down', (opt) => {
-            console.log(opt.absolutePointer)
-            })
-            editor.canvas.on('mouse:up', (opt) => {
-            console.log(opt.absolutePointer)
-            editor.canvas.selectionColor = originalColor;
-            //editor.canvas.selection = true;
-            })
-        }*/
-    }
-
     return (
         <>
-            <Typography variant="h6">Column 2</Typography>
+            <Typography variant="h6">Main Canvas</Typography>
             <Card sx={{border: '1px solid green', borderRadius: 2}} variant="outlined">
-                    <button onClick={onAddCircle}>Add circle</button>
-                    <button onClick={onAddRectangle}>Add Rectangle</button>
-                    <button onClick={onMore}>Test</button>
                 <FabricJSCanvas className="sample-canvas" onReady={myOnReady} />
             </Card>
         </>
